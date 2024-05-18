9626868ab588e543fdd8e21cafd7ae3a66439855
Brian <sbriansantos2002@gmail.com>
2024-05-17 21:00:32 -0300

    feat: page sign up structured

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  PoFieldModule,
+  PoModalModule,
+  PoButtonModule,
+  PoLinkModule,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [
+    NgOptimizedImage,
+    PoFieldModule,
+    PoModalModule,
+    PoButtonModule,
+    PoLinkModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  signUpForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    confirmTerms: new FormControl(false),
+  });
+
+  errorPassword = this.signUpForm.value.password;
+}
